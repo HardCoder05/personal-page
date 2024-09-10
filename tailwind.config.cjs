b2ef5e0c62c394d4fce884480911cfd28ceba112
rio <rio88920@gmail.com>
2024-09-10 15:39:24 -0500

    Refactor tailwind.config.cjs and tsconfig.json

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -14,5 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography", "flowbite/plugin"), animations],
+  plugins: [require("@tailwindcss/typography"), animations],
 };
